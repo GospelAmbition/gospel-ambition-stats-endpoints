7570fa9eca2a13d6dec1eae10e20196b27bffd9e
Chris Chasm <chris@chasm.solutions>
2023-02-06 12:58:05 -0700

    phpcs

--- zume_vision/add-registrations.php
+++ zume_vision/add-registrations.php
@@ -10,18 +10,18 @@ add_filter( 'go_stats_endpoint', function( $stats ) {
         'value' => $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->users;" ),
     ];
 
-    $twelve_months_ago = date( 'Y-m-d', strtotime( '-12 months' ) );
+    $twelve_months_ago = gmdate( 'Y-m-d', strtotime( '-12 months' ) );
     $stats['registrations_last_12_months'] = [
         'label' => 'Registrations in the last 12 months',
         'description' => 'The number of registrations who have registered in the last 12 months to the Zume.Training system.',
-        'value' => $wpdb->get_var( $wpdb->prepare(  "SELECT COUNT(*) FROM $wpdb->users WHERE user_registered > %s;", $twelve_months_ago) ),
+        'value' => $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM $wpdb->users WHERE user_registered > %s;", $twelve_months_ago ) ),
     ];
 
-    $thirty_days_ago = date( 'Y-m-d', strtotime( '-30 days' ) );
+    $thirty_days_ago = gmdate( 'Y-m-d', strtotime( '-30 days' ) );
     $stats['registrations_last_30_days'] = [
         'label' => 'Registrations in the last 30 days',
         'description' => 'The number of registrations who have registered in the last 30 days to the Zume.Training system.',
-        'value' => $wpdb->get_var( $wpdb->prepare(  "SELECT COUNT(*) FROM $wpdb->users WHERE user_registered > %s;", $thirty_days_ago) ),
+        'value' => $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM $wpdb->users WHERE user_registered > %s;", $thirty_days_ago ) ),
     ];
 
     return $stats;
--- zume_vision/add-trainings.php
+++ zume_vision/add-trainings.php
@@ -7,7 +7,7 @@ add_filter( 'go_stats_endpoint', function( $stats ) {
     $stats['training_groups_formed'] = [
         'label' => 'Training Groups Formed with Zume.Training System',
         'description' => 'Total number of trainings formed with the Zume.Training system. This does not include groups that reused the same training group, or groups that were formed but did not use the Zume.Training system.',
-        'value' => $wpdb->get_var( $wpdb->prepare(  "SELECT COUNT(*) FROM $wpdb->usermeta WHERE meta_key LIKE 'zume_group%';", $wpdb->esc_like( 'zume_group' ) . '%' ) ),
+        'value' => $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM $wpdb->usermeta WHERE meta_key LIKE %s;", $wpdb->esc_like( 'zume_group' ) . '%' ) ),
     ];
 
     return $stats;
