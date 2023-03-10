a72b2c1d60c083a1de52d2aa17028a20b159aafa
corsacca <corsacca@gmail.com>
2023-03-10 16:01:30 +0100

    phpcs

--- assets/stats-pages.php
+++ assets/stats-pages.php
@@ -6,6 +6,6 @@ add_action('wp_enqueue_scripts', function (){
 
     if ( isset( $_SERVER['REQUEST_URI'] ) && strpos( sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) ), 'stats' ) !== false ){
         wp_enqueue_style( 'stats-pages', plugin_dir_url( __FILE__ ) . 'stats-page.css', [], filemtime( plugin_dir_path( __FILE__ ) . 'stats-page.css' ) );
-        wp_enqueue_style( 'material-font-icons', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.6.96/css/materialdesignicons.min.css' );
+        wp_enqueue_style( 'material-font-icons', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.6.96/css/materialdesignicons.min.css', [], '6.6.96' );
     }
 });
