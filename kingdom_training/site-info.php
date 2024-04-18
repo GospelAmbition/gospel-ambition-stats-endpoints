f86f6993ecc28923451d9ac71bbd66f1769e58d8
corsacca <corsacca@gmail.com>
2024-04-18 13:23:46 +0100

    update typo

--- kingdom_training/site-info.php
+++ kingdom_training/site-info.php
@@ -4,7 +4,7 @@ if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
 add_filter( 'go_site_info', function( $stats ) {
     $stats['site_name'] = get_bloginfo();
     $stats['icon'] = GO_Context_Switcher::plugin_url( '/assets/icons/kt-circle-logo.png' );
-    $stats['site_description'] = 'https//kingdom.training equips disciple makers to accelerate movements with an end-to-end media strategy.';
+    $stats['site_description'] = 'https://kingdom.training equips disciple makers to accelerate movements with an end-to-end media strategy.';
 
     return $stats;
 }, 10, 1 );
